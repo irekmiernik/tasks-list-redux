cba01408c9b4abe2761f77569f10ed8bf788297b
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2024-12-21 08:01:02 +0100

    Refactorization

--- src/features/tasksList/getExamplesTasks.js
+++ src/features/tasksList/getExamplesTasks.js
@@ -2,7 +2,7 @@ export const getExamplesTasks = async () => {
   const response = await fetch("/tasks-list-redux/examplesTasks.json");
   if (!response.ok) {
     new Error(response.statusText);
-  }
+  };
 
   return await response.json();
 
