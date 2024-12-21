849459e9e48071fd518b7dc2cc453bab2deb54c5
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2024-12-21 07:35:29 +0100

    Removed component Body

--- src/common/Body.js
+++
@@ -1,3 +0,0 @@
-export const Body = ({ children }) => (
-    <main>{children} </main>
-);
--- src/features/tasksList/Main.js
+++ src/features/tasksList/Main.js
@@ -1,9 +1,9 @@
 import { useSelector } from 'react-redux';
 import { tasksState } from './tasksListSlice';
-import { Body } from '../../common/Body';
 import { Header } from '../../common/Header';
 import { Section } from '../../common/Section';
 import { AddTask } from './AddTask';
+import { OneButton } from './OneButton';
 import { TwoButtons } from './TwoButtons';
 import { TasksList } from './TasksList';
 
@@ -11,10 +11,10 @@ export const Main = () => {
   localStorage.setItem("tasksTable", JSON.stringify(useSelector(tasksState).tasksTable));
 
   return (
-    <Body>
+    <main>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<AddTask />} />
+      <Section title="Dodaj nowe zadanie" oneButton={<OneButton />} body={<AddTask />} />
       <Section title="Lista zadań" twoButtons={<TwoButtons />} body={<TasksList />} />
-    </Body>
+    </main>
   );
 };
